3dd7ecaafbf36ff0487567421af22ca1d5994981
AJ ONeal <coolaj86@gmail.com>
2013-06-26 13:52:33 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+Purpose
+===
+
+Turn gists into flash cards.
+
+Usage
+===
+
+Drag 
+<a href='javascript:function%20shuffle(e)%7Bvar%20t=e.length,n,r;while(t)%7Br=Math.floor(Math.random()*t--);n=e%5Bt%5D;e%5Bt%5D=e%5Br%5D;e%5Br%5D=n%7Dreturn%20e%7Dfunction%20getNodes(e)%7Bvar%20t,n=%5B%5D,r=e.nextSibling;$(e).hide();for(t=0;t%3C30;t+=1)%7Bif(null===r%7C%7C%22h3%22===r.nodeName.toLowerCase())%7Bbreak%7Dif(%22h2%22===r.nodeName.toLowerCase())%7Br=r.nextSibling;continue%7D$(r).hide();n.push(r);r=r.nextSibling%7Dreturn%20n%7Dfunction%20showCard()%7Bflash=flashes.pop();ans=flash.a.slice(0);$(flash.q).show()%7Dfunction%20showCardPart()%7Bvar%20e;if(ans.length)%7Be=ans.shift();while(e&&/%5Es*$/.test($(e).text()))%7Be=ans.shift()%7D$(e).show();return%7D$(flash.q).hide();$(flash.a).hide();showCard()%7Dvar%20cards,flashes=%5B%5D;$($(%22.context-loader-container%22)%5B0%5D).find(%22h2%22).hide();cards=$($(%22.context-loader-container%22)%5B0%5D).find(%22h3%22);cards=%5B%5D.slice.call(cards);cards.forEach(function(e)%7Bflashes.push(%7Bq:e,a:getNodes(e)%7D)%7D);console.log(flashes);var%20flash=flashes.sort(shuffle).pop(),ans=%5B%5D;showCard();$($(%22.context-loader-container%22)%5B0%5D).on(%22click%22,function()%7BshowCardPart()%7D)'>this bookmarklet</a>
+to your bookmarks bar and then click on it.
+
+Alternatively, you can copy and paste the script here into the JavaScript console on any gist that is in the correct format.
+
+Format
+===
+
+Any gist that is written in the following format can be used with this javascript.
+
+```markdown
+Title: Pop-Quiz
+===
+
+Category: Monty Python & the Holy Grail
+---
+
+### Question: What is your favorite color?
+
+Answer: Blue... no, YELLOOOOOOOW!
+
+### Question: What is the average airspeed velocity of an unladen swallow?
+
+11 m/s
+
+Source: <http://style.org/unladenswallow/>
+```
---
+++ gistcards.js
@@ -0,0 +1,101 @@
+(function () {
+  "use strict";
+
+  // this code is terribad, please don't try to learn from it
+  // I do plan to clean it up, but it's very dirty right now
+  var cards
+    , flashes = []
+    ;
+    
+  function shuffle(array) {
+    var m = array.length, t, i;
+   
+    // While there remain elements to shuffle…
+    while (m) {
+   
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+   
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+   
+    return array;
+  }
+   
+  function getNodes(child) {
+    var i
+      , siblings = []
+      , sibling = child.nextSibling
+      ;
+   
+    $(child).hide();
+    
+    for (i = 0; i < 30; i += 1) {
+      if (null === sibling || 'h3' === sibling.nodeName.toLowerCase()) {
+        break;
+      }
+      // ignore categories for now
+      if ('h2' === sibling.nodeName.toLowerCase()) {
+        sibling = sibling.nextSibling;
+        continue;
+      }
+      $(sibling).hide();
+      siblings.push(sibling);
+      sibling = sibling.nextSibling;
+    }
+    return siblings;
+  }
+   
+  // hide categories
+  $($('.context-loader-container')[0]).find('h2').hide();
+  cards = $($('.context-loader-container')[0]).find('h3');
+  cards = [].slice.call(cards);
+   
+  cards.forEach(function (card) {
+    flashes.push({
+      q: card
+    , a: getNodes(card)
+    });
+  });
+   
+  function runCardApp() {
+    var flash = flashes.sort(shuffle).pop()
+      , ans = []
+      ;
+     
+    function showCard() {
+      flash = flashes.pop();
+      ans = flash.a.slice(0);
+      $(flash.q).show();
+      //$(flash.a).show();
+    }
+    function showCardPart() {
+      var a
+        ;
+        
+      if (ans.length) {
+        a = ans.shift();
+        while (a && /^\s*$/.test($(a).text())) {
+          a = ans.shift();
+        }
+        $(a).show();
+        return;
+      }
+      $(flash.q).hide();
+      $(flash.a).hide();
+      
+      showCard();
+    }
+     
+    showCard();
+    $($('.context-loader-container')[0]).on('click', function () {
+      showCardPart();
+    });
+  }
+  runCardApp();
+
+  console.log("We're straight. You can close the JavaScript console now.");
+}());
